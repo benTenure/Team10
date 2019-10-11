89be899eee0007ba5937be50ee2b57dadf791c58
tmirenzi <mirenzit@gmail.com>
2019-10-10 22:36:57 -0400

    #2: added a vue cli3 boilerplate w webpack and some other modules we plan to use such as vuex and axios

---
+++ frontend/vue-phaser3-master/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Portal />
+</template>
+
+
+<script>
+export default {
+  name: 'App',
+  components: { Portal }
+}
+</script>
+
+
+<style lang="scss">
+body {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+}
+</style>
