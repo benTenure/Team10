8b3b3450f096182d6d6e6c458918e7c2de932c5d
ed765super <ogbonna1@umbc.edu>
2019-11-10 16:43:31 -0500

    Got the dates into the filters, made checkboxes with the filters. time is wonkey, but the date is fire

--- frontend/my-project/src/components/Home.vue
+++ frontend/my-project/src/components/Home.vue
@@ -2,110 +2,132 @@
   <div>
     <h1>{{ msg }}</h1>
     <!--  Start of nick's timeframe (ctrl-frwdslash makes comment)-->
-    <div class="text-left">
-      <v-btn class="ma-2" color="blue" dark>FILTERS
-      </v-btn>
-      <v-flex >
-        <v-menu
-          v-model="menu2"
-          :close-on-content-click="true"
-          :nudge-right="40"
-          lazy
-          transition="scale-transition"
-          offset-y
-          full-width
-          min-width="280px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="date"
-              label="Date From"
-              readonly
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
-        </v-menu>
-      </v-flex>
+    <v-row justify="center">
+      <v-dialog v-model="dialog" scrollable max-width="250px">
+        <template v-slot:activator="{ on }">
+          <v-btn color="blue darken-2" dark v-on="on">Filters</v-btn>
+        </template>
+        <v-card>
+          <div class="text-left">
+            <v-flex >
+              <v-menu
+                v-model="menu2"
+                :close-on-content-click="true"
+                :nudge-right="40"
+                lazy
+                transition="scale-transition"
+                offset-y
+                full-width
+                min-width="280px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="date"
+                    label="Date From"
+                    readonly
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+              </v-menu>
+            </v-flex>
 
-      <v-flex >
-        <v-menu
-          v-model="menu2"
-          :close-on-content-click="true"
-          :nudge-right="40"
-          lazy
-          transition="scale-transition"
-          offset-y
-          full-width
-          max-width="280px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="date"
-              label="Date To"
-              readonly
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker v-model="date" @input="menu2 = false">
+            <v-flex >
+              <v-menu
+                v-model="menu2"
+                :close-on-content-click="true"
+                :nudge-right="40"
+                lazy
+                transition="scale-transition"
+                offset-y
+                full-width
+                max-width="280px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="date"
+                    label="Date To"
+                    readonly
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker v-model="date" @input="menu2 = false">
+                  <v-spacer></v-spacer>
+                </v-date-picker>
+              </v-menu>
+            </v-flex>
+          </div>
+          <v-row>
+            <v-menu
+              ref="menu"
+              v-model="menu2"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              :return-value.sync="time"
+              transition="scale-transition"
+              offset-y
+              full-width
+            >
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  v-model="time"
+                  label="Time From"
+                  readonly
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-time-picker
+                v-if="menu2"
+                v-model="time"
+                full-width
+                @click:minute="$refs.menu.save(time)"
+              ></v-time-picker>
+            </v-menu>
             <v-spacer></v-spacer>
-          </v-date-picker>
-        </v-menu>
-      </v-flex>
-    </div>
-    <v-row>
-        <v-menu
-          ref="menu"
-          v-model="menu2"
-          :close-on-content-click="false"
-          :nudge-right="40"
-          :return-value.sync="time"
-          transition="scale-transition"
-          offset-y
-          max-width="290px"
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="time"
-              label="Time From"
-              readonly
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-time-picker
-            v-if="menu2"
-            v-model="time"
-            full-width
-            @click:minute="$refs.menu.save(time)"
-          ></v-time-picker>
-        </v-menu>
-      <v-spacer></v-spacer>
-        <v-menu
-          ref="dialog"
-          v-model="modal2"
-          :return-value.sync="time"
-          width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="time"
-              label="Time To"
-              readonly
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-time-picker
-            v-if="modal2"
-            v-model="time"
-            full-width
-          >
-            <v-spacer></v-spacer>
-            <v-btn text color="blue" @click="modal2 = false">Cancel</v-btn>
-            <v-btn text color="blue" @click="$refs.dialog.save(time)">OK</v-btn>
-          </v-time-picker>
-        </v-menu>
+            <v-menu
+              ref="dialog"
+              v-model="modal2"
+              :return-value.sync="time"
+            >
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  v-model="time"
+                  label="Time To"
+                  readonly
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-time-picker
+                v-if="modal2"
+                v-model="time"
+                full-width
+              >
+                <v-spacer></v-spacer>
+                <v-btn text color="blue" @click="modal2 = false">Cancel</v-btn>
+                <v-btn text color="blue" @click="$refs.dialog.save(time)">OK</v-btn>
+              </v-time-picker>
+            </v-menu>
+          </v-row>
+          <v-card-title>Select Filter</v-card-title>
+          <v-divider></v-divider>
+          <v-card-text style="height: 300px;">
+            <v-radio-group v-model="dialogm1" column>
+              <v-checkbox label="Colorblind Mode" value="colorblind"></v-checkbox>
+              <v-checkbox label="Show Weapons" value="weapons"></v-checkbox>
+              <v-checkbox label="Show Crime Codes" value="crimeCodes"></v-checkbox>
+              <v-checkbox label="Show Locations" value="weapons"></v-checkbox>
+            </v-radio-group>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+          </v-card-actions>
+
+        </v-card>
+      </v-dialog>
     </v-row>
+
     <v-flex>
       <div class="chart">
         <h2>Linechart example with fake raw data; crimes by district over</h2>
@@ -148,6 +170,8 @@ export default {
     msg: 'Team 10\'s 447 project template',
     time: '',
     modal2: '',
+    dialogm1: '',
+    dialog: false,
     options: {
       responsive: true,
       maintainAspectRatio: false
