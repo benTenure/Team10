2d834424fe4a1d79d6f8662451a1d7f5e62db6d2
tmirenzi <mirenzit@gmail.com>
2019-11-24 21:41:19 -0500

    #24: Data is showing up on the map, however the icons aren't working so im using the default blue circles

--- frontend/my-project/src/components/Map.vue
+++ frontend/my-project/src/components/Map.vue
@@ -1,7 +1,12 @@
 <template>
   <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true" style="height: 400px">
     <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation" projection="EPSG:4326"></vl-view>
-
+    <vl-feature v-for="crime in crimePoints" :key="crime.id">
+      <vl-geom-point :coordinates="crime.coords"></vl-geom-point>
+<!--      <vl-style-box>-->
+<!--        <vl-style-icon src="./marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>-->
+<!--      </vl-style-box>-->
+    </vl-feature>
     <vl-layer-tile>
       <vl-source-osm></vl-source-osm>
     </vl-layer-tile>
@@ -13,9 +18,21 @@ export default {
   name: 'mapDataSet',
   data () {
     return {
-      zoom: 11.5,
-      center: ['-76.6566890000', '39.3265610000'],
-      rotation: 0
+      zoom: 11,
+      center: [-76.6566890000, 39.3265610000],
+      rotation: 0,
+      crimePoints: []
+    }
+  },
+  created () {
+    for (let crime of this.$store.state.chartExampleData) {
+      let value = {
+        id: crime.id,
+        crimedate: crime.crimedate,
+        crimetime: crime.crimetime,
+        coords: [crime.longitude, crime.latitude]
+      }
+      this.crimePoints.push(value)
     }
   }
 }
